658f74b3c106cdfe63dd5ee3152f28aede677caf
Ogefest <hello@ogefest.com>
2021-09-12 23:42:15 +0200

    config clear

--- src/main/java/com/ogefest/filehunter/Configuration.java
+++ src/main/java/com/ogefest/filehunter/Configuration.java
@@ -1,50 +1,56 @@
 package com.ogefest.filehunter;
 
-import org.ini4j.Ini;
+//import org.ini4j.Ini;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class Configuration {
     private String filename;
-    private Ini ini;
+//    private Ini ini;
 
     public Configuration(String filename) throws IOException {
         this.filename = filename;
 
-        ini = new Ini();
-        ini.load(new FileReader(filename));
+//        ini = new Ini();
+//        ini.load(new FileReader(filename));
+
+    }
+
+    public Configuration() {
 
     }
 
     public String getValue(String key) {
         HashMap<String, String> data = new HashMap<>();
-        data.put("storage.directory", "/tmp/filehunterstorage");
+//        data.put("storage.directory", "./filehunterstorage");
+        data.put("storage.directory", new File("filehunterstorage").getAbsolutePath());
 
         return data.get(key);
     }
 
-    public ArrayList<Directory> getDirectoriesFromConfig() {
-
-        ArrayList<Directory> result = new ArrayList<>();
-
-        for (String sectionName : ini.keySet()) {
-
-            Directory d = new Directory();
-            d.setName(sectionName);
-
-            Map<String, String> map = ini.get(sectionName);
-
-            ArrayList<String> pathList = new ArrayList<>();
-            for (String elem : Arrays.asList(map.get("path").split(","))) {
-                pathList.add(elem);
-            }
-            d.setPath(pathList);
-
-            result.add(d);
-        }
-
-        return result;
-    }
+//    public ArrayList<Directory> getDirectoriesFromConfig() {
+//
+//        ArrayList<Directory> result = new ArrayList<>();
+//
+//        for (String sectionName : ini.keySet()) {
+//
+//            Directory d = new Directory();
+//            d.setName(sectionName);
+//
+//            Map<String, String> map = ini.get(sectionName);
+//
+//            ArrayList<String> pathList = new ArrayList<>();
+//            for (String elem : Arrays.asList(map.get("path").split(","))) {
+//                pathList.add(elem);
+//            }
+//            d.setPath(pathList);
+//
+//            result.add(d);
+//        }
+//
+//        return result;
+//    }
 }
