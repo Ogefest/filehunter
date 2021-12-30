6f341288a0a7fef38f09d434026233f0930083a2
Ogefest <hello@ogefest.com>
2021-12-30 23:33:55 +0100

    httpok version

--- src/main/java/com/ogefest/filehunter/SystemStatus.java
+++ src/main/java/com/ogefest/filehunter/SystemStatus.java
@@ -1,8 +1,14 @@
 package com.ogefest.filehunter;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 public class SystemStatus {
+
     private long docsNumber = 0;
     private String currentTask = "";
+
+    @ConfigProperty(name = "quarkus.application.version")
+    private String currentVersion;
 
     public long getDocsNumber() {
         return docsNumber;
@@ -18,5 +24,13 @@ public class SystemStatus {
 
     public void setCurrentTask(String taskName) {
         this.currentTask = taskName;
+    }
+
+    public String getCurrentVersion() {
+        return currentVersion;
+    }
+
+    public void setCurrentVersion(String currentVersion) {
+        this.currentVersion = currentVersion;
     }
 }
